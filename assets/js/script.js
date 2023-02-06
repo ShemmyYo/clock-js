1832842945732749ba18f81340a3b99e1a4df742
ShemmyYo <rabiliamemo@gmail.com>
2023-02-06 20:12:34 +0000

    js sctipt complete

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]') 
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsTime = currentDate.getSeconds() / 60
+    const minutesTime = (secondsTime + currentDate.getMinutes()) / 60
+    const hoursTime = (minutesTime + currentDate.getHours()) / 12
+
+    setRotation(secondHand, secondsTime)
+    setRotation(minuteHand, minutesTime)
+    setRotation(hourHand, hoursTime)
+}
+
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
